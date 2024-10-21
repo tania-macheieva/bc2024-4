0f17df51357ada31228eaa693dbf145e074a5663
tania-macheieva bc2024-4 <macheieva.tetiana@gmail.com>
2024-10-21 15:38:59 +0300

    Realization of the method GET~

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { Command } = require('commander');
 const http = require('http');
+const fs = require('fs').promises;
+const path = require('path');
+
 const program = new Command();
 
 program
@@ -10,10 +13,24 @@ program
 
 const { host, port, cache } = program.opts();
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Server is running');
-});
+const server = http.createServer(async (req, res) => {
+  const filePath = path.join(cache, req.url.substring(1) + '.jpg');
+
+  if (req.method === 'GET') {
+    try {
+      const data = await fs.readFile(filePath);
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+      res.end(data);
+    } catch {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not Found');
+    }
+  } else {
+    // Відповідь для не підтримуваних методів
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.end('Method Not Allowed');
+  }
+}); 
 
 server.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}/`);
