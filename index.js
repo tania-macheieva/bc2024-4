c3315b23caf5e5b68bcfffedd56703cc4f2bd85a
tania-macheieva bc2024-4 <macheieva.tetiana@gmail.com>
2024-10-21 12:02:38 +0300

    Implement command-line parameters and basic server setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Command } = require('commander');
+const http = require('http');
+const program = new Command();
+
+program
+  .requiredOption('-h, --host <host>', 'server host')
+  .requiredOption('-p, --port <port>', 'server port')
+  .requiredOption('-c, --cache <path>', 'cache directory path')
+  .parse(process.argv);
+
+const { host, port, cache } = program.opts();
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Server is running');
+});
+
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
+});
