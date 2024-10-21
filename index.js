654b2025f5770c8b658a366621af1fa366e3d1a3
tania-macheieva bc2024-4 <macheieva.tetiana@gmail.com>
2024-10-21 15:44:37 +0300

    Final commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Command } = require('commander');
 const http = require('http');
 const fs = require('fs').promises;
 const path = require('path');
-
+const superagent = require('superagent');
 const program = new Command();
 
 program
@@ -16,32 +16,41 @@ const { host, port, cache } = program.opts();
 const server = http.createServer(async (req, res) => {
   const filePath = path.join(cache, req.url.substring(1) + '.jpg');
 
+  // Обробка GET запиту
   if (req.method === 'GET') {
     try {
       const data = await fs.readFile(filePath);
       res.writeHead(200, { 'Content-Type': 'image/jpeg' });
       res.end(data);
     } catch {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Not Found');
+      // Якщо зображення не знайдено, перевірте http.cat
+      const statusCode = req.url.substring(1);
+      try {
+        const response = await superagent.get(`https://http.cat/${statusCode}`);
+        await fs.writeFile(filePath, response.body);
+        res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+        res.end(response.body);
+      } catch {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+      }
     }
-  } else if (req.method === 'PUT') {
-    // Обробка запиту PUT - запис файлу в кеш
+  }
+
+  // Обробка PUT запиту
+  else if (req.method === 'PUT') {
     const buffers = [];
     req.on('data', chunk => buffers.push(chunk));
     req.on('end', async () => {
       const data = Buffer.concat(buffers);
-      try {
-        await fs.writeFile(filePath, data);
-        res.writeHead(201, { 'Content-Type': 'text/plain' });
-        res.end('Created');
-      } catch (error) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-      }
+      await fs.writeFile(filePath, data);
+      res.writeHead(201, { 'Content-Type': 'text/plain' });
+      res.end('Created');
     });
-  }  else if (req.method === 'DELETE') {
-    // Обробка запиту DELETE - видалення файлу з кешу
+  }
+
+  // Обробка DELETE запиту
+  else if (req.method === 'DELETE') {
     try {
       await fs.unlink(filePath);
       res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -50,12 +59,14 @@ const server = http.createServer(async (req, res) => {
       res.writeHead(404, { 'Content-Type': 'text/plain' });
       res.end('Not Found');
     }
-  } else {
-    // Відповідь для не підтримуваних методів
+  }
+
+  // Інші методи
+  else {
     res.writeHead(405, { 'Content-Type': 'text/plain' });
     res.end('Method Not Allowed');
   }
-}); 
+});
 
 server.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}/`);
